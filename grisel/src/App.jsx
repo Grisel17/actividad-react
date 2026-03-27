e176427f04a464e2a6a90d9ef434f396377eefae
Grisel Silva <grisel_silva17@hotmail.com>
2026-03-26 21:28:44 -0700

    Integracion de tabla Mui

--- grisel/src/App.jsx
+++ grisel/src/App.jsx
@@ -3,6 +3,36 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: '#6b46c1',
+    color: theme.palette.common.white,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+}));
+
+const personas = [
+  { nombre: "Guadalupe", apellido: "Sànchez", edad: 55 },
+  { nombre: "Antonio", apellido: "Silva", edad: 60 },
+  { nombre: "Kenia", apellido: "García", edad: 25 },
+  { nombre: "Brandon", apellido: "Hernández", edad: 17 },
+  { nombre: "Ariadna", apellido: "Martínez", edad: 15 },
+  { nombre: "Grisel", apellido: "Silva", edad: 22 }
+];
 
 function App() {
   const [count, setCount] = useState(0)
@@ -48,8 +78,60 @@ function App() {
           Decrementar
         </button>
       </section>
+
+      <CustomizedTables />
     </>
   )
+}
+
+function CustomizedTables() {
+  return (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Nombre</StyledTableCell>
+            <StyledTableCell>Apellido</StyledTableCell>
+            <StyledTableCell align="right">Edad</StyledTableCell>
+          </TableRow>
+        </TableHead>
+
+        <TableBody>
+          {personas.map((persona, index) => {
+            const esMenor = persona.edad <= 17;
+
+            return (
+              <StyledTableRow
+                key={index}
+                sx={{
+                  backgroundColor: esMenor ? '#ffebee' : 'inherit' // fondo rojo claro
+                }}
+              >
+                <StyledTableCell
+                  sx={{ color: esMenor ? 'red' : 'inherit', fontWeight: esMenor ? 'bold' : 'normal' }}
+                >
+                  {persona.nombre}
+                </StyledTableCell>
+
+                <StyledTableCell
+                  sx={{ color: esMenor ? 'red' : 'inherit' }}
+                >
+                  {persona.apellido}
+                </StyledTableCell>
+
+                <StyledTableCell
+                  align="right"
+                  sx={{ color: esMenor ? 'red' : 'inherit' }}
+                >
+                  {persona.edad}
+                </StyledTableCell>
+              </StyledTableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
 
 export default App
